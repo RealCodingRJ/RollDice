706d330b2495a9963d8953df423219d80221561e
Priates of Zimsey <realcoder2023@gmail.com>
2025-10-13 22:15:23 -0400

    added dice roll game code

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use rand::{random_range};
+
+fn main() {
+
+    let number = random_range(1..6);
+
+    if number == 1 {
+        println!("You ROLLED: {:?}", number);
+    }
+
+    else if number == 2 {
+        println!("You ROLLED: {:?}", number);
+    }
+
+    else if number == 3 {
+        println!("You ROLLED: {:?}", number);
+    }
+
+    else if number == 4 {
+        println!("You ROLLED: {:?}", number);
+    }
+
+    else if number == 5 {
+        println!("You ROLLED: {:?}", number);
+    }
+
+    else {
+        println!("You ROLLED: {:?}", number);
+    }
+}

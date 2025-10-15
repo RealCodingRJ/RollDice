575fd81c2605b4ae895f8c0c91159528c365f31e
Priates of Zimsey <realcoder2023@gmail.com>
2025-10-14 21:51:56 -0400

    added tests to app to check if number is eual to 4 or any number

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
-use rand::{random_range};
+use rand::random_range;
+
+pub fn rand_num() -> i32 {
+    let number = random_range(1..6);
+    return number;
+}
 
 fn main() {
-
-    let number = random_range(1..6);
+    let number = rand_num();
 
     if number == 1 {
         println!("You ROLLED: {:?}", number);
-    }
-
-    else if number == 2 {
+    } else if number == 2 {
+        println!("You ROLLED: {:?}", number);
+    } else if number == 3 {
+        println!("You ROLLED: {:?}", number);
+    } else if number == 4 {
+        println!("You ROLLED: {:?}", number);
+    } else if number == 5 {
+        println!("You ROLLED: {:?}", number);
+    } else {
         println!("You ROLLED: {:?}", number);
     }
+}
 
-    else if number == 3 {
-        println!("You ROLLED: {:?}", number);
-    }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    else if number == 4 {
-        println!("You ROLLED: {:?}", number);
-    }
-
-    else if number == 5 {
-        println!("You ROLLED: {:?}", number);
-    }
-
-    else {
-        println!("You ROLLED: {:?}", number);
+    #[test]
+    fn it_works() {
+        let number = rand_num();
+        assert_eq!(number, 4);
     }
 }

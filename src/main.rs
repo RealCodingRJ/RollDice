fd310a23f377c5fc058f7392a8b1d5d40f256c60
Priates of Zimsey <realcoder2023@gmail.com>
2025-10-14 22:34:35 -0400

    added result trait to code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
+use core::panic;
+
 use rand::random_range;
 
-pub fn rand_num() -> i32 {
+pub fn rand_num() -> Result<(), i32> {
     let number = random_range(1..6);
-    return number;
+
+    if number == 4 {
+        Ok(())
+    } else {
+        panic!("Not a Number")
+    }
 }
 
 fn main() {
     let number = rand_num();
 
-    if number == 1 {
+    if number == Err(1) {
         println!("You ROLLED: {:?}", number);
-    } else if number == 2 {
+    } else if number == Err(2) {
         println!("You ROLLED: {:?}", number);
-    } else if number == 3 {
+    } else if number == Err(3) {
         println!("You ROLLED: {:?}", number);
-    } else if number == 4 {
+    } else if number == Err(4) {
         println!("You ROLLED: {:?}", number);
-    } else if number == 5 {
+    } else if number == Err(5) {
         println!("You ROLLED: {:?}", number);
     } else {
         println!("You ROLLED: {:?}", number);
@@ -28,8 +35,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works() -> Result<(), i32> {
         let number = rand_num();
-        assert_eq!(number, 4);
+
+        match number {
+            Ok(_) => Ok(()),
+            Err(_) => todo!(),
+        }
     }
 }
